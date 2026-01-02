5ce9943a1a00391053c7747413d81429772780e7
Piyush-13090 <piyushsharma130906@gmail.com>
2026-01-03 04:38:04 +0530

    backend server created

---
+++ server/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000; 
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
